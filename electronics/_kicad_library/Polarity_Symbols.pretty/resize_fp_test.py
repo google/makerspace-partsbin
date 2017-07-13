4fae67dad563d11cb0cb9c2fca81d396e52a532c
Tim Hatch <thatch@google.com>
2017-07-13 10:47:58 -0700

    Rename polarity symbols library, fix test

--- electronics/_kicad_library/Polarity_Symbols.pretty/resize_fp_test.py
+++ electronics/_kicad_library/Polarity_Symbols.pretty/resize_fp_test.py
@@ -1,3 +1,5 @@
+# This file is intended to be run by nosetests.
+#
 import resize_fp
 
 def test_fixer():
@@ -6,7 +8,7 @@ def test_fixer():
   assert resize_fp.fixer(resize_fp.RE.match("xy 1.0 2.0")) == "xy 2.0 4.0"
   assert resize_fp.RE.sub(resize_fp.fixer, "xy 1.0 2.0") == "xy 2.0 4.0"
   a = "pts (xy 0.284256 -3.001128) (xy 0.547425 -2.965017)"
-  b = "pts (xy 0 -6) (xy 1 -6)"
+  b = "pts (xy 0.568512 -6.002256) (xy 1.09485 -5.930034)"
   assert resize_fp.RE.sub(resize_fp.fixer, a) == b
 
 
